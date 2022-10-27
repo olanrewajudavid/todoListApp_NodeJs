fc93728ae5dd627c17abb76b39539c38d32219f0
David Owopetu <olanrewajudavid@gmail.com>
2022-10-27 11:01:55 +0100

    First Commit

---
+++ date.js
@@ -0,0 +1,18 @@
+module.exports = getDate;
+
+console.log(module)
+
+function getDate() {
+
+    let today = new Date()
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    
+    let day = today.toLocaleDateString("en-US", options) 
+    return day
+
+}
